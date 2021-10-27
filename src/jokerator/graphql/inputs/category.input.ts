8111f58f21dd0422f1423c5e9f47a8fca753fa7d
Denis Yakovlev <deni-goodman@tutanota.de>
2021-10-28 02:56:55 +0700

    style: remove comment

--- src/jokerator/graphql/inputs/category.input.ts
+++ src/jokerator/graphql/inputs/category.input.ts
@@ -2,12 +2,6 @@ import { Field, InputType } from '@nestjs/graphql'
 
 import { CategoryEnum } from '../enums/category.enum'
 
-// @InputType()
-// export class CategoryInput {
-//     @Field(() => [CategoryEnum], { defaultValue: CategoryEnum.Any, nullable: true })
-//     categories: CategoryEnum[]
-// }
-
 @InputType()
 export class CategoryInput {
     @Field(() => [CategoryEnum], {
