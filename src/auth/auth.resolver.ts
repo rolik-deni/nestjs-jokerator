01c619a807010e836b169a9f2dce2f129276e6ad
Denis Yakovlev <deni-goodman@tutanota.de>
2021-11-15 23:00:21 +0700

    refactor: implement logical mutations

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -2,12 +2,17 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 
 import { AuthType } from 'src/auth/graphql/types/auth.type'
 import { UserInput } from 'src/users/dto/user.input'
+import { UserType } from 'src/users/graphql/types/user.type'
+import { UsersService } from 'src/users/users.service'
 
 import { AuthService } from './auth.service'
 
 @Resolver(() => String)
 export class AuthResolver {
-    constructor(private readonly _authService: AuthService) {}
+    constructor(
+        private readonly _authService: AuthService,
+        private readonly _usersService: UsersService,
+    ) {}
 
     @Query(() => AuthType, { description: 'Authorization' })
     async signIn(@Args('input') input: UserInput): Promise<AuthType> {
@@ -23,5 +28,10 @@ export class AuthResolver {
             refreshToken,
             accessToken,
         )
+    }
+
+    @Mutation(() => UserType, { name: 'signUp', description: 'Registration' })
+    async createUser(@Args('input') input: UserInput): Promise<UserType> {
+        return await this._usersService.create(input)
     }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { User } from './entities/user.entity'
-import { UsersResolver } from './users.resolver'
 import { UsersService } from './users.service'
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
-    providers: [UsersResolver, UsersService],
+    providers: [UsersService],
     exports: [UsersService],
 })
 export class UsersModule {}
